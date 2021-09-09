531a70a40480d5bf123f07cd8a05e3a3837f185a
DarraghReid <darragh.reid@gmail.com>
2021-09-09 09:23:24 +0000

    adjust fields display in admin

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,31 @@ from django.contrib import admin
 from .models import Product
 from .models import Category
 
+
 # Register models
-admin.site.register(Product)
-admin.site.register(Category)
+# ModelAdmin is built in
+class ProductAdmin(admin.ModelAdmin):
+    # list_display is tuple which tells admin which fields
+    # to display in the admin
+    list_display = (
+        'sku',
+        'name',
+        'description',
+        'price',
+        'rating',
+        'image',
+        'image_url',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+# Register models along with their respective classes
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
