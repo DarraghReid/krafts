4ea2bb82433e3211d13775e0b4f7cd86e5d0e412
DarraghReid <darragh.reid@gmail.com>
2021-09-20 14:10:20 +0000

    add adjust & remove from cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -33,3 +33,49 @@ def add_to_cart(request, item_id):
 
     # Redirect user back to redirect_url
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust item quantity """
+
+    # Get item quantity from quantity selector form input field
+    quantity = int(request.POST.get('quantity'))
+
+    # Get or create session dictionary to store cart contents
+    cart = request.session.get('cart', {})
+
+    # Data coming from quantity selector form in cart.html
+    # If there is more than one of a specific item
+    if quantity > 0:
+        # Set the item's quantity to that number
+        cart[item_id] = quantity
+    # Otherwise, remove the item
+    else:
+        cart.pop(item_id)
+
+    # Put cart variable into session
+    request.session['cart'] = cart
+
+    # Redirect user back to cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Remove item from cart """
+
+    try:
+        # Get or create session dictionary to store cart contents
+        cart = request.session.get('cart', {})
+
+        # Remove item from cart
+        cart.pop(item_id)
+
+        # Put cart variable into session
+        request.session['cart'] = cart
+
+        # Return http response if successfully removed
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        # Return any exception as 500 error
+        return HttpResponse(status=500)
