c024d091cb0a3aa246f1a0790f6fbe0e7f303cfd
DarraghReid <darragh.reid@gmail.com>
2021-09-30 09:54:18 +0000

    update country form field, placeholder

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,28 +25,29 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # full_name autofocus attribute set to true, so cursor starts here
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            # If the form field is required
-            if self.fields[field].required:
-            # Place a star next to it to indicate this
-                placeholder = f'{placeholders[field]} *'
-            else:
-                # Otherwise, there is no star
-                placeholder = placeholders[field]
-            # Set form placeholder attributes to respective values in placeholder dict above
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # Ensure that code doesn't seek 'country' field
+            if field != 'country':
+                # If the form field is required
+                if self.fields[field].required:
+                    # Place a star next to it to indicate this
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    # Otherwise, there is no star
+                    placeholder = placeholders[field]
+                # Set form placeholder attributes to respective
+                # values in placeholder dict above
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # Add stripe-style-input CSS class to each field
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             # Removing label from each field
             self.fields[field].label = False
-            
