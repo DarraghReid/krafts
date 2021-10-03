07c79b40115b3578ab745a5d5349e7e8b1f56c93
DarraghReid <darragh.reid@gmail.com>
2021-10-03 15:14:24 +0000

    add product form

---
+++ products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    # Meta class defines model and fields
+    class Meta:
+        model = Product
+        # Dunder string includes all fields
+        fields = '__all__'
+
+    # Override __intit__ method
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Get all categories
+        categories = Category.objects.all()
+        # Create list of tuples of categories' friendly names
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        # Use friendly names in form instead of name field
+        self.fields['category'].choices = friendly_names
+        # Add 'border-black rounded-0' classes to match site styles
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
