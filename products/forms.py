e0e7207ecf50452b4c9da6e546abc7143e7b3549
DarraghReid <darragh.reid@gmail.com>
2021-10-11 15:17:25 +0000

    fix some site wide line too long bugs, comments

--- products/forms.py
+++ products/forms.py
@@ -4,22 +4,27 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    """ Create Product form using Django instead of directly in the HTML.
+        ProductForm to be imported and rendered in views.py """
 
     # Meta class defines model and fields
     class Meta:
+        # Tell form which model it is associated with
         model = Product
+        # Tell form which fields is should render:
         # Dunder string includes all fields
         fields = '__all__'
 
     # Replace form image field with image that uses widget
-    image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput)
 
-    # Override __intit__ method
+    # Override __intit__ method to customize category spelling
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Get all categories
         categories = Category.objects.all()
-        # Create list of tuples of categories' friendly names
+        # Get each category's friendly name, insert into tuple
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         # Use friendly names in form instead of name field
