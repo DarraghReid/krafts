29239a46c2125d54313d0ed7003a2aeb64ac4ecf
DarraghReid <darragh.reid@gmail.com>
2021-10-13 15:29:43 +0000

    create Comment model

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from .utils import generate_sku
 
-# Each product will have a category which follows this model
+
 class Category(models.Model):
-    # Meta class for admin to display verbose name of this model
+    """ Define structure of Category model in db """
     class Meta:
+        """ Define verbose name of this model for admin display """
         verbose_name_plural = "Categories"
 
     # Character field representing programmatic name
@@ -21,15 +22,15 @@ class Category(models.Model):
         return self.friendly_name
 
 
-# Each product will be added according to this model
 class Product(models.Model):
+    """ Define structure of Product model in db """
     # category is foreign key to Category model
-    # If category deleted, its products' category will be null
-    # Only name, description, price and required
+    # If category deleted, its product's category will be null
     category = models.ForeignKey(
         "Category", null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(
         max_length=254, null=True, blank=True, default=generate_sku)
+    # Only name, description, price and required
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -41,3 +42,20 @@ class Product(models.Model):
     # String method takes in Product model, returns model name
     def __str__(self):
         return self.name
+
+
+# Each comment will be added according to this model
+class Comment(models.Model):
+    """ Define structure of Comment model in db """
+    # product is foreign key to Product model
+    # If a product is deleted, associated comments will also be deleted
+    product = models.ForeignKey(
+        "Product", related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    comment = models.TextField(max_length=500)
+    # Automatically add date to comment
+    date = models.DateTimeField(auto_now_add=True)
+
+    # String method takes in Comment model, returns model name
+    def __str__(self):
+        return '%s - %s' % (self.product.name, self.name)
