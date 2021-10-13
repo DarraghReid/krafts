46db099900e7630e9f1d219c8349a01d1237e5b1
DarraghReid <darragh.reid@gmail.com>
2021-10-13 18:35:54 +0000

    add comments section to product_detail page

--- products/admin.py
+++ products/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 from .models import Product
 from .models import Category
+from .models import Comment
 
 
-# ModelAdmin is built in
 class ProductAdmin(admin.ModelAdmin):
-    # list_display tuple tells admin which fields to display in admin
+    """ Tell admin which fields to display in admin """
     list_display = (
         'pk',
         'sku',
@@ -22,11 +22,24 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    """ Tell admin which fields to display in admin """
     list_display = (
         'name',
         'friendly_name',
     )
 
+
+class CommentAdmin(admin.ModelAdmin):
+    """ Tell admin which fields to display in admin """
+    list_display = (
+        'name',
+        'date',
+        'product',
+        'comment',
+    )
+
+
 # Register models along with their respective classes
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
