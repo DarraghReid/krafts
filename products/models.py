ce91dc48918730dd0a3f96a90fa37e68ea6961d0
DarraghReid <darragh.reid@gmail.com>
2021-10-16 20:00:15 +0000

    add rates field to product model

--- products/models.py
+++ products/models.py
@@ -39,6 +39,14 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    rates = models.ManyToManyField(User, related_name="products")
+
+    @property
+    def rating_average(self):
+        """ Return list of child comments of a parent comment
+            to be accessed in the template """
+        average = self.rating / self.rates
+        return average
 
     # String method takes in Product model, returns model name
     def __str__(self):
