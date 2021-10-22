a6890c673e9e1abcd319875f6fce54c1e76a3d07
DarraghReid <darragh.reid@gmail.com>
2021-10-22 21:15:09 +0000

    refactor progress bar

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,6 @@
 // Get progress bar that contains cart total as data attribute
 let progressBar = document.getElementById("progress-bar");
-console.log("progressbar: " + progressBar)
+console.log("progressbar: " + progressBar);
 
 // Get cart total from data attribute
 let total = Math.floor(progressBar.dataset.total)
@@ -11,8 +11,11 @@ percentage = total * 2
 console.log("Percentage: " + percentage)
 
 // Get inner div of progress bar
-let percentageBar = document.getElementById("percentage")
+let percentageBar = document.getElementById("percentage");
 console.log("percentageBar: " + percentageBar)
 
 // Set percentage width of inner div to delivery threshold percentage
 percentageBar.style.width = `${percentage}%`
+
+// Pad free delivery span relative to percentage bar
+document.getElementById("free-delivery").style.paddingLeft = `${percentage}%`;
